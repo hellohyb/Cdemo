2b22f5aa9afdd65569544142f6e0c01e61142dbb
hello-hyb <1713296741@qq.com>
2022-10-20 21:06:27 +0800

    链表

--- LinkNode.cpp
+++ LinkNode.cpp
@@ -2,23 +2,40 @@
 // Created by Huang on 2022/10/20 0020.
 //
 #include<iostream>
-
 using namespace std;
-
 typedef struct Link{
     int data;
-    int* next;
+    struct Link* next;
 }Link;
-void initLink(){
-    Link* L = new Link;
-    L->data = 0;
-
+//初始化链表
+void initLink(Link &L){
+    L.data = 0;
+    L.next = NULL;
+}
+//头插法
+void insertHead(Link* L, int data){
+    Link* node = new Link;
+    node->data = data;
+    node->next = L->next;
+    L->next = node;
+}
+//打印输出链表
+void print(Link* L){
+    Link* node = L->next;
+    while(node){
+        cout<<node->data<<"->";
+        node = node->next;
+    }
+    cout<<"NULL";
 }
 int main(){
     Link* l1 = new Link;
-    l1->data = 0;
-    l1->next = NULL;
-    cout<<l1->data;
-
+    initLink(*l1);
+    insertHead(l1,1);
+    insertHead(l1,2);
+    insertHead(l1,3);
+    insertHead(l1,4);
+    insertHead(l1,5);
+    print(l1);
     return 0;
 }
