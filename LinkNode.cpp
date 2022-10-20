8340472edd0ae03873e687c98654b93bf8b76266
hello-hyb <1713296741@qq.com>
2022-10-20 21:21:31 +0800

    单链表

--- LinkNode.cpp
+++ LinkNode.cpp
@@ -19,6 +19,32 @@ void insertHead(Link* L, int data){
     node->next = L->next;
     L->next = node;
 }
+//尾插法
+void insertTail(Link* L, int data){
+    Link* node = L;
+    //遍历到最后
+    while(node->next){
+        node = node->next;
+    }
+    //定义新结点，放在node（此时node结点已经在尾端）后面
+    Link* newNode = new Link;
+    newNode->data = data;
+    newNode->next = NULL;
+    node->next = newNode;
+}
+// 删除元素
+bool deleteElement(Link* L, int data){
+    Link* node = L->next;
+    while(node){
+        if(node->data == data){
+            L->next = node->next;
+            delete(node);
+            return true;
+        }
+        L = node;
+        node = node->next;
+    }
+}
 //打印输出链表
 void print(Link* L){
     Link* node = L->next;
@@ -37,6 +63,9 @@ int main(){
     insertHead(l1,4);
     insertHead(l1,5);
     insertHead(l1,6);
+    deleteElement(l1,3);
+    deleteElement(l1,1);
+    insertTail(l1,100);
     print(l1);
     return 0;
 }
