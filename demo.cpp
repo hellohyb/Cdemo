38b8e009e1e9dfbf3e912e5104651024c9eb94c1
hello-hyb <1713296741@qq.com>
2022-10-27 21:31:10 +0800

    设置壁纸，更新

--- demo.cpp
+++ demo.cpp
@@ -4,11 +4,8 @@
 
 #include<iostream>
 #include<Windows.h>
-#include<commdlg.h>
-#include<string>
 using namespace std;
-
-int main(){
+void setWallpaper(){
     OPENFILENAME ofn;      // 公共对话框结构。
     TCHAR szFile[MAX_PATH]; // 保存获取文件名称的缓冲区。
     // 初始化选择文件对话框。
@@ -19,12 +16,12 @@ int main(){
     ofn.lpstrFile[0] = '\0';
     ofn.nMaxFile = sizeof(szFile);
     ofn.lpstrFilter = reinterpret_cast<LPCSTR>((LPCWSTR) "*.jpg\0*.jpeg\0*.png\0\0");
-    ofn.nFilterIndex = 1;
-    ofn.lpstrFileTitle = NULL;
-    ofn.nMaxFileTitle = 0;
-    ofn.lpstrInitialDir = NULL;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-    //ofn.lpTemplateName =  MAKEINTRESOURCE(ID_TEMP_DIALOG);
+//    ofn.nFilterIndex = 1;
+//    ofn.lpstrFileTitle = NULL;
+//    ofn.nMaxFileTitle = 0;
+//    ofn.lpstrInitialDir = NULL;
+//    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+//    ofn.lpTemplateName =  MAKEINTRESOURCE(ID_TEMP_DIALOG);
     // 显示打开选择文件对话框。
     if ( GetOpenFileName(&ofn) )
     {
@@ -32,15 +29,22 @@ int main(){
         cout << szFile << endl;
         // 设置壁纸
         const char* path = szFile;
-        SystemParametersInfoA(0x0014,0,(PVOID)path,0x0002);
+//      SystemParametersInfoA(0x0014,0,(PVOID)path,0x0002);
+        //SPIF_UPDATEINIFILE永久设置
+        SystemParametersInfoA(SPI_SETDESKWALLPAPER,0,(PVOID)path,SPIF_UPDATEINIFILE);
         //
+        MessageBox(NULL,TEXT("壁纸设置成功！"),TEXT("成功"),MB_OK);
         cout<<"壁纸设置成功！";
         //
-        OutputDebugString(szFile);    //这一句是显示路径吗？为什么不显示？
-        OutputDebugString(reinterpret_cast<LPCSTR>((LPCWSTR) "\r\n"));
+//        OutputDebugString(szFile);    //这一句是显示路径吗？为什么不显示？
+//        OutputDebugString(reinterpret_cast<LPCSTR>((LPCWSTR) "\r\n"));
     }else{
         cout<<"未选择文件！";
     }
 
+}
+int main(){
+    setWallpaper();
+//    SystemParametersInfoA(0x0014,0,(PVOID)"https://www.bing.com/th?id=OHR.BridgeofSighs_ZH-CN5414607871_1920x1080.jpg",0x0002);
     return 0;
 }
